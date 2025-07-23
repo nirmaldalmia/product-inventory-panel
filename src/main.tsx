ca575b26ed9e631a27835df614ab4b0063ba66ea
Nirmal Dalmia <nirmaldalmia17@gmail.com>
2025-07-23 18:38:38 +0530

    setup dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider } from '@tanstack/react-router'
 import './index.css'
-import App from './App.tsx'
+import { router } from './router'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootElement = document.getElementById('root')!
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>,
+  )
+}
